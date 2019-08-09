80fefcd9afe2369128633430e8abeab2a786fac5
alfrednoble <alfrednoblex@gmail.com>
2019-08-09 01:20:37 +0100

    add firebase cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.validateAlias = functions.https.onCall((data, context) => {
+    const ref = admin.firestore().collection('users').doc(data.slug);
+
+    return ref.get().then(doc => {
+        return { unique: !doc.exists }
+    }).catch(err => {
+        throw new functions.https.HttpsError('Unable to connect')
+    })
+})
+
